3b53a98b2fb4720bb518e9ed08b15c2db5fcd7cb
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-01 20:20:16 +0900

    feat: rentalLog component

--- src/components/rentalList/rentalLog/RentalLog.js
+++ src/components/rentalList/rentalLog/RentalLog.js
@@ -1,9 +1,110 @@
 import Search from "components/search/Search";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { SiMicrosoftexcel } from "react-icons/si";
+// import "../RentalList.scss";
+import DetailEquipment from "components/detail/DetailEquipment";
+import { Link } from "react-router-dom";
+import * as XLSX from "xlsx";
+import { useHeaderActive } from "hooks/useActive";
 
 export default function RentalLog() {
+    const ExcelExport = () => {
+        const table = document.getElementById("equipment-list");
+        const wb = XLSX.utils.table_to_book(table, {
+            raw: true
+        });
+
+        XLSX.writeFile(wb, "기자재리스트.xlsx");
+    }
+    const [rentalList, setRentalList] = useState();
+    const [rentalListPage, setRentalListPage] = useState(1);
+    const getRentalList = async () => {
+        await axios.get(`http://120.142.105.189:5080/tool/viewToolList/1/${rentalListPage}`)
+            .then(res => {
+                if (res.data.suc) {
+                    setRentalList(res.data.result);
+                }
+                else Promise.reject(new Error("rentalList API 호출 실패"));
+            }).catch(err => {
+                console.log("rentalList API 오류", err);
+            });
+    }
+
+    useEffect(() => {
+        getRentalList(1, rentalListPage);
+    }, [rentalListPage]);
+
     return (
-        <>
-            <Search />
-        </>
+        <div style={{
+            width: "100%",
+            height: "804px",
+            position: "relative"
+        }}>
+            <div id="contents-header">
+                <Link to="/home/rentalList"
+                    className={useHeaderActive("/home/rentalList") ? "active" : null}>
+                    대여 목록
+                </Link>
+                <Link to="/home/rentalList/rentalLog" className={useHeaderActive("/home/rentalList/rentalLog") ? "active" : null}>
+                    대여 로그
+                </Link>
+                <Search />
+                {/*여기 엑셀 버튼을 나중에 컴포넌트로 따로 분리해주기 바람
+                이유는 나중에 엑셀 export해주기 위해!! */}
+                <SiMicrosoftexcel size="27px" color="#20744A" onClick={ExcelExport} />
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th className="check-wrap">
+                            <input type="checkbox" id="check-btn" />
+                            <label htmlFor="check-btn" />
+                        </th>
+                        <th>구분</th>
+                        <th>대여자명</th>
+                        <th>기자재명</th>
+                        <th>자산 번호</th>
+                        <th>대여 일자</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="check-wrap">
+                            <input type="checkbox" id="check-btn" />
+                            <label htmlFor="check-btn" />
+                        </td>
+                        <td>교육용</td>
+                        <td>김홍길동</td>
+                        <td>스마트 패드</td>
+                        <td>2017021402226</td>
+                        <td>11 / 21</td>
+                    </tr>
+                </tbody>
+            </table>
+            <footer className="list-nav">
+                <p>
+                    &lt;
+                </p>
+                {/* <ul style={{
+                    display: "inline-flex",
+                            listStyle: "none",
+                }}>
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                            <li>5</li>
+                        </ul> */}
+                <button onClick={() => setRentalListPage(1)}>1</button>
+                <button onClick={() => setRentalListPage(2)}>2</button>
+                <button onClick={() => setRentalListPage(2)}>3</button>
+                <button onClick={() => setRentalListPage(2)}>4</button>
+                <button onClick={() => setRentalListPage(2)}>5</button>
+                <p>
+                    &gt;
+                </p>
+            </footer>
+        </div>
     );
 }
