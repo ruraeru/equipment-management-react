fe0ee01c7de28c8eaa55c6b1a1699a06e5dda9ae
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-01 20:20:39 +0900

    feat: path header active hook

--- src/hooks/useActive.js
+++ src/hooks/useActive.js
@@ -2,6 +2,9 @@ const useActive = (path) => {
     return window.location.pathname.startsWith(path);
 }
 
+const useHeaderActive = (path) => {
+    return !window.location.pathname.localeCompare(path);
+}
 export {
-    useActive
+    useActive, useHeaderActive
 }
