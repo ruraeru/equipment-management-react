f318c784cf186085a9f673f007819ebe6592b256
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-02 16:33:07 +0900

    feat: 기자재 자세히 보기 구현 중

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,14 +1,20 @@
 import axios from "axios";
-import { useState } from "react";
-import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom";
+import "./Login.scss";
 
 export default function Login() {
-    const [token, setToken] = useState();
+    const [cookies, setCookie, removeCookie] = useCookies(['token']);
     const [input, setInput] = useState({
         user_id: "",
         user_pw: ""
     });
     const { user_id, user_pw } = input;
+
+    useEffect(() => {
+        removeCookie('token');
+    }, []);
 
     const onInputChange = (e) => {
         const { name, value } = e.target;
@@ -25,11 +31,11 @@ export default function Login() {
                 user_pw: user_pw
             }
         ).then(res => {
-            // if (res) {
-            console.log(res.data);
-            localStorage.setItem("token", res.data.token.token);
-            // }
-            // else Promise.reject(new Error("API 반환값 에러"));
+            if (res.data.suc) {
+                console.log(res.data);
+                setCookie('token', res.data.token.token);
+            }
+            else Promise.reject(new Error(res.data.err));
         }).catch(err => Promise.reject(new Error("서버 에러", err)));
     }
 
@@ -40,7 +46,7 @@ export default function Login() {
                     tool_id: "test1"
                 },
                 headers: {
-                    token: localStorage.getItem("token")
+                    token: cookies.token
                 }
             }).catch(err => console.log(err));
     }
@@ -50,36 +56,38 @@ export default function Login() {
         onLogin();
     }
     return (
-        <>
-            <LoginFormWrap onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    name="user_id"
-                    value={user_id}
-                    autoComplete="none"
-                    placeholder="아이디"
-                    onChange={onInputChange} />
-                <input
-                    type="password"
-                    name="user_pw" value={user_pw}
-                    autoComplete="none"
-                    placeholder="비밀번호"
-                    onChange={onInputChange} />
-                <input type="submit" />
-            </LoginFormWrap>
-            <button onClick={onDetailEquipment}>기자재 자세히 보기</button>
-        </>
+        <div>
+            <div>
+                {cookies?.token}
+                <button onClick={onDetailEquipment}>토큰 인증</button>
+                <h1>로그인</h1>
+                <form onSubmit={onSubmit}>
+                    <p>아이디</p>
+                    <input
+                        name="user_id"
+                        placeholder="학번 혹은 아이디를 입력해주세요."
+                        value={input.user_id}
+                        onChange={onInputChange} />
+                    <p>비밀번호</p>
+                    <input
+                        name="user_pw"
+                        placeholder="비밀번호를 입력해주세요."
+                        value={input.user_pw}
+                        onChange={onInputChange} />
+                    <input type="submit" value="로그인" />
+                </form>
+                <ul>
+                    <li>
+                        <Link to="/user/findid">아이디 찾기</Link>
+                    </li>
+                    <li>
+                        <Link to="/user/modifypass">비밀번호 변경</Link>
+                    </li>
+                    <li>
+                        <Link to="/user/signup">회원가입</Link>
+                    </li>
+                </ul>
+            </div>
+        </div >
     );
 }
-
-const LoginFormWrap = styled.form`
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-
-   input {
-    width: 220px;
-    height: 36px;
-   }
-`;
