ff4f1aeee242115ee76e944a967f4d278ddeeb0d
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-06 18:01:31 +0900

    style: 품명 추가

--- src/components/tool/ScreenData.js
+++ src/components/tool/ScreenData.js
@@ -1,13 +1,13 @@
 export default function ScreenData({ json }) {
     return (
         json.map((item, index) => {
-            console.log(item);
             return (
                 <tr key={index}>
                     <td>{item.자산번호}</td>
                     <td>{item.관리부서}</td>
                     <td>{item.구분}</td>
                     <td>{item.품목코드}</td>
+                    <td>{item.품명}</td>
                     <td>{item.규격}</td>
                     <td>{item.구입일자}</td>
                     <td>{item.구입구분}</td>
