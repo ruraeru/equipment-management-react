5c9fd7d56fbf889dee82b49a88ed5bf44700c9f4
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-06 18:03:29 +0900

    feat: 유저 라이센스에 따라 표시하는 내용이 다름, 내 대여 목록 구현

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,8 +1,38 @@
+import axios from "axios";
 import { useActive } from "hooks/useActive";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Profile.scss";
 
 export default function Profile({ userData }) {
+    const [userType, setUserType] = useState("");
+    const [rentalList, setRentalList] = useState();
+    const getMyRentalList = async () => {
+        await axios.get(`${process.env.REACT_APP_DOMAIN}/rental/myCurrentRentalList/student/1`)
+            .then((res) => {
+                setRentalList(res.data);
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+    useEffect(() => {
+        getMyRentalList();
+    }, []);
+    useEffect(() => {
+        switch (userData.login.user_license) {
+            case 1:
+                setUserType("마스터");
+                break;
+            case 2:
+                setUserType("관리자");
+                break;
+            case 3:
+                setUserType("학부생");
+                break;
+            default:
+                break;
+        }
+    }, []);
     return (
         <div style={{
             display: "flex",
@@ -20,7 +50,7 @@ export default function Profile({ userData }) {
                     <p>
                         <b>{userData.login.user_name}</b> 님
                     </p>
-                    <p>(관리자)</p>
+                    <p>({userType})</p>
                 </div>
                 <hr />
                 <div id="navigation">
@@ -34,73 +64,29 @@ export default function Profile({ userData }) {
                         className={useActive("/tool/changeInfo") ? "active" : null}>기자재 정보 수정</Link>
                     <Link to="/tool/addTool"
                         className={useActive("/tool/addTool") ? "active" : null}>기자재 추가</Link>
+                    {userType === "마스터" &&
+                        <Link to={"/user/authorization"}
+                            // eslint-disable-next-line react-hooks/rules-of-hooks
+                            className={useActive("/user/authorization") ? "active" : null}>회원가입 승인</Link>}
                 </div>
             </div>
             {/*추후 분리 예정 */}
 
             <div id="myRentalList">
                 <h4>내 대여 목록</h4>
-                <div>
-                    <p style={{
-                        fontSize: "16px",
-                        color: "#E03333",
-                    }}>D - 1</p>
-                    <p>
-                        스마트 패드 <br />
-                        <span>품목 코드 : 9115</span>
-                    </p>
-                </div>
-                <div>
-                    <p style={{
-                        fontSize: "16px",
-                        color: "#E03333",
-                    }}>D - 1</p>
-                    <p>
-                        스마트 패드 <br />
-                        <span>품목 코드 : 9115</span>
-                    </p>
-                </div>
-                <div>
-                    <p style={{
-                        fontSize: "16px",
-                        color: "#E03333",
-                    }}>D - 1</p>
-                    <p>
-                        스마트 패드 <br />
-                        <span>품목 코드 : 9115</span>
-                    </p>
-                </div>
-                <div>
-                    <p style={{
-                        fontSize: "16px",
-                        color: "#E03333",
-                    }}>D - 1</p>
-                    <p>
-                        스마트 패드 <br />
-                        <span>품목 코드 : 9115</span>
-                    </p>
-                </div>
-                <div>
-                    <p style={{
-                        fontSize: "16px",
-                        color: "#E03333",
-                    }}>D - 1</p>
-                    <p>
-                        스마트 패드 <br />
-                        <span>품목 코드 : 9115</span>
-                    </p>
-                </div>
-                <div>
-                    <p style={{
-                        fontSize: "16px",
-                        color: "#E03333",
-                    }}>D - 1</p>
-                    <p>
-                        스마트 패드 <br />
-                        <span>품목 코드 : 9115</span>
-                    </p>
-                </div>
+                {rentalList && rentalList.map((item, index) => (
+                    <div key={index}>
+                        <p style={{
+                            fontSize: "16px",
+                            color: "#E03333",
+                        }}>{item.D_day}</p>
+                        <p>
+                            {item.result.tool.tool_name} <br />
+                            <span>품목 코드 : {item.result.tool.tool_id}</span>
+                        </p>
+                    </div>
+                ))}
             </div>
-        </div>
+        </div >
     );
 }
