6da645008b5ce3d0b88d45a393c5da3a64fcd8d4
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-06 18:03:51 +0900

    feat: 내 대여 목록 API 연동

--- src/components/myRentalList/MyRentalList.js
+++ src/components/myRentalList/MyRentalList.js
@@ -1,8 +1,22 @@
+import axios from "axios";
 import Search from "components/search/Search";
 import { useHeaderActive } from "hooks/useActive";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function MyRentalList() {
+    const [rentalList, setRentalList] = useState();
+    const getMyRentalList = async () => {
+        await axios.get(`${process.env.REACT_APP_DOMAIN}/rental/myCurrentRentalList/student/1`)
+            .then((res) => {
+                setRentalList(res.data);
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+    useEffect(() => {
+        getMyRentalList();
+    }, []);
     return (
         <>
             <div id="contents-header">
@@ -26,14 +40,21 @@ export default function MyRentalList() {
                         <th>관리 부서</th>
                         <th>기자재명</th>
                         <th>자산 번호</th>
-                        <th>기자재 상태</th>
+                        <th>대여 기간</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr onClick={(e) => {
-                        console.log(e.target.parentElement.remove());
-                        e.target.parentNode.append(12)
-                    }}>
+                    {rentalList && rentalList.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.result.rental_date.split("-")[1] + " / " + item.result.rental_date.split("-")[2].slice(0, 2)}</td>
+                            <td>{item.result.tool.tool_use_division}</td>
+                            <td>{item.result.tool.department.department_name}</td>
+                            <td>{item.result.tool.tool_name}</td>
+                            <td>{item.result.tool.tool_id}</td>
+                            <td>{item.D_day}</td>
+                        </tr>
+                    ))}
+                    <tr>
                         <td>11 / 16</td>
                         <td>교육용</td>
                         <td>소프트웨어콘텐츠 과</td>
