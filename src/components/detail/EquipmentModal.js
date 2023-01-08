83cb67d67d0269fefb4fb6425212daaa465b82df
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-08 16:33:20 +0900

    refactor: equipment modal

---
+++ src/components/detail/EquipmentModal.js
@@ -0,0 +1,26 @@
+import "./EquipmentModal.scss";
+
+export default function EquipmentModal({ open, close, header, children, data }) {
+    return (
+        <div className={open ? "openModal modal" : "modal"}>
+            {open && (
+                <div>
+                    <header>
+                        {data.result.tool_name}
+                        <button className="close" onClick={close}>
+                            &times;
+                        </button>
+                    </header>
+                    <main>
+                        {children}
+                    </main>
+                    <footer>
+                        <button className="close" onClick={close}>
+                            닫기
+                        </button>
+                    </footer>
+                </div>
+            )}
+        </div>
+    )
+}
