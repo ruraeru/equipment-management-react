9627e0dab9880289fbce0306ea8eaf022a43cdcc
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-11 19:59:51 +0900

    feat: 체크된 항목만 excel로 export

--- src/components/rentalList/RentalList.js
+++ src/components/rentalList/RentalList.js
@@ -3,15 +3,49 @@ import axios from "axios";
 import Search from "components/search/Search";
 import { SiMicrosoftexcel } from "react-icons/si";
 import "./RentalList.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import * as XLSX from "xlsx";
-import { useHeaderActive } from "hooks/useActive";
+import { useActive, useHeaderActive } from "hooks/useActive";
 import DetailEquipment from "components/detail/DetailEquipment";
 import Modal from "components/detail/EquipmentModal";
+import Navigation from "components/nav/Navigation";
 
 export default function RentalList({ userData }) {
+    const page = useParams().page;
+    const navigate = useNavigate();
     const [modalOpen, setModalOpen] = useState(false);
     const [modalData, setModalData] = useState();
+
+    const [checkItems, setCheckItems] = useState([]);
+
+    // 체크박스 단일 선택
+    const handleSingleCheck = (checked, id) => {
+        if (checked) {
+            // 단일 선택 시 체크된 아이템을 배열에 추가
+            setCheckItems(prev => [...prev, id]);
+        } else {
+            // 단일 선택 해제 시 체크된 아이템을 제외한 배열 (필터)
+            setCheckItems(checkItems.filter((el) => el !== id));
+        }
+        console.log(checkItems);
+    };
+
+    // 체크박스 전체 선택
+    const handleAllCheck = (checked) => {
+        if (checked) {
+            // 전체 선택 클릭 시 데이터의 모든 아이템(id)를 담은 배열로 checkItems 상태 업데이트
+            const idArray = [];
+            rentalList.forEach((el) => {
+                console.log(el.tool_id);
+                idArray.push(el.tool_id);
+            });
+            setCheckItems(idArray);
+        }
+        else {
+            // 전체 선택 해제 시 checkItems 를 빈 배열로 상태 업데이트
+            setCheckItems([]);
+        }
+    }
 
     const setDetailEquipmentData = async (tool_id) => {
         await axios.get(`${process.env.REACT_APP_DOMAIN}/tool/viewTool`, {
@@ -49,16 +83,17 @@ export default function RentalList({ userData }) {
         XLSX.writeFile(wb, "기자재리스트.xlsx");
     }
     const [rentalList, setRentalList] = useState();
-    const [rentalListPage, setRentalListPage] = useState(1);
     const getRentalList = async () => {
-        console.log(userData.token)
-        await axios.get(`${process.env.REACT_APP_DOMAIN}/tool/viewToolList/1/${rentalListPage}`, {
+        console.log(userData.login.user_license);
+        console.log(userData.token);
+        //${userData.login.user_license}
+        await axios.get(`${process.env.REACT_APP_DOMAIN}/tool/viewToolList/1/${page}`, {
             headers: {
                 token: userData.token
             }
         })
             .then(res => {
-                console.log(res);
+                console.log(res.data.result);
                 if (res.data.suc) {
                     setRentalList(res.data.result);
                 }
@@ -69,8 +104,8 @@ export default function RentalList({ userData }) {
     }
 
     useEffect(() => {
-        getRentalList(1, rentalListPage);
-    }, [rentalListPage]);
+        getRentalList(1, page);
+    }, [page]);
 
     return (
         <div style={{
@@ -79,8 +114,8 @@ export default function RentalList({ userData }) {
             position: "relative"
         }}>
             <div id="contents-header">
-                <Link to="/home/rentalList"
-                    className={useHeaderActive("/home/rentalList") ? "active" : null}>
+                <Link to="/home/rentalList/1"
+                    className={useActive("/home/rentalList") ? "active" : null}>
                     대여 목록
                 </Link>
                 <Link to="/home/rentalList/rentalLog" className={useHeaderActive("/home/rentalList/rentalLog") ? "active" : null}>
@@ -91,12 +126,14 @@ export default function RentalList({ userData }) {
                 이유는 나중에 엑셀 export해주기 위해!! */}
                 <SiMicrosoftexcel size="27px" color="#20744A" onClick={ExcelExport} />
             </div>
-            <table id="equipment-list">
+            <table >
                 <thead>
                     <tr>
-                        <th className="check-wrap">
-                            <input type="checkbox" id="check-btn" />
-                            <label htmlFor="check-btn" />
+                        <th>
+                            <input type="checkbox" name="select-all"
+                                onChange={(e) => handleAllCheck(e.target.checked)}
+                                checked={checkItems.length === rentalList?.length ? true : false}
+                            />
                         </th>
                         <th>구분</th>
                         <th>관리 부서</th>
@@ -107,12 +144,16 @@ export default function RentalList({ userData }) {
                 </thead>
                 <tbody>
                     {rentalList && rentalList.map((item, index) => (
-                        <tr key={index} onClick={() => {
-                            setDetailEquipmentData(item.tool_id);
+                        <tr key={index} onClick={(e) => {
+                            if (e.target.tagName !== "INPUT") {
+                                setDetailEquipmentData(item.tool_id);
+                            }
                         }}>
-                            <td className="check-wrap">
-                                <input type="checkbox" id="check-btn" />
-                                <label htmlFor="check-btn" />
+                            <td>
+                                <input type="checkbox" name={`select-${item.tool_id}`}
+                                    onChange={(e) => handleSingleCheck(e.target.checked, item.tool_id)}
+                                    checked={checkItems.includes(item.tool_id) ? true : false}
+                                />
                             </td>
                             <td>{item.tool_use_division}</td>
                             <td>{item.department.department_name}</td>
@@ -127,40 +168,46 @@ export default function RentalList({ userData }) {
                             }>{item.tool_state}</td>
                         </tr>
                     ))}
-                    {/* <DetailEquipment /> */}
-                    {/* <tr onClick={() => {
-                        openModal();
-                        setDetailEquipmentData("test");
-                    }}>
-                        <td className="check-wrap">
-                            <input type="checkbox" id="check-btn" />
-                            <label htmlFor="check-btn" />
-                        </td>
-                        <td>교육용</td>
-                        <td>소프트웨어콘텐츠 과</td>
-                        <td>스마트 패드</td>
-                        <td>2017021402226</td>
-                        <td>대여 가능</td>
-                    </tr> */}
-                    {/* <DetailEquipment /> */}
+                </tbody>
+            </table>
+            <table style={{
+                display: "none"
+            }} id="equipment-list">
+                <thead>
+                    <tr>
+                        <th>구분</th>
+                        <th>관리 부서</th>
+                        <th>기자재명</th>
+                        <th>자산 번호</th>
+                        <th>기자재 상태</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {checkItems?.map((item_id) => (
+                        rentalList?.map((item, index) => (
+                            item_id === item.tool_id ?
+                                <tr key={index}>
+                                    <td>{item.tool_use_division}</td>
+                                    <td>{item.department.department_name}</td>
+                                    <td>{item.tool_name}</td>
+                                    <td>{item.tool_id}</td>
+                                    <td className={
+                                        item.tool_state === "대여가능"
+                                            ? "rentalT"
+                                            : item.tool_state === "대여 중"
+                                                ? "rentalI"
+                                                : "rentalF"
+                                    }>{item.tool_state}</td>
+                                </tr>
+                                : null
+                        ))
+                    ))}
                 </tbody>
             </table>
             <Modal open={modalOpen} close={closeModal} data={modalData}>
                 <DetailEquipment data={modalData} />
             </Modal>
-            <footer className="list-nav">
-                <p>
-                    &lt;
-                </p>
-                <button onClick={() => setRentalListPage(1)}>1</button>
-                <button onClick={() => setRentalListPage(2)}>2</button>
-                <button onClick={() => setRentalListPage(2)}>3</button>
-                <button onClick={() => setRentalListPage(2)}>4</button>
-                <button onClick={() => setRentalListPage(2)}>5</button>
-                <p>
-                    &gt;
-                </p>
-            </footer>
+            <Navigation list={["/home/rentalList/1", "/home/rentalList/2", "/home/rentalList/3", "/home/rentalList/4", "/home/rentalList/5",]} />
         </div >
     );
 }
