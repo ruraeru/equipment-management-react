8c4a8e4fd021d2326fa74adc11f70b513ca802ca
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-12 18:42:02 +0900

    feat: 기자재 이미지가 있다면 불러와서 출력, 대여자가 있다면 대여자 정보도 같이 출력

--- src/components/detail/DetailEquipment.js
+++ src/components/detail/DetailEquipment.js
@@ -3,8 +3,9 @@ import { useEffect } from "react";
 import "./DetailEquipment.scss";
 
 export default function DetailEquipment({ data }) {
-    console.log(data);
-    const { tool_name, tool_code, tool_id, tool_purchase_division, tool_purchase_date, tool_standard }
+    console.log("data", data);
+    const { rental_date, rental_due_date } = data.rental;
+    const { tool_name, tool_code, tool_id, tool_purchase_division, tool_purchase_date, tool_standard, tool_state }
         = data.result;
     useEffect(() => {
         loadEquipmentImg();
@@ -12,7 +13,9 @@ export default function DetailEquipment({ data }) {
 
     const loadEquipmentImg = async () => {
         const img = document.querySelector("img");
-        img.src = `${process.env.REACT_APP_DOMAIN}/tool/${data.image.img_url}`;
+        if (data.image) {
+            img.src = `${process.env.REACT_APP_DOMAIN}/tool/${data.image.img_url}`;
+        }
     }
     return (
         <div id="detail-equipment" style={{
@@ -21,10 +24,10 @@ export default function DetailEquipment({ data }) {
             <div>
                 <img src="https://www.lenovo.com/medias/lenovo-tablet-lenovo-tab-p12-pro-subseries-hero.png?context=bWFzdGVyfHJvb3R8MjM1NTEwfGltYWdlL3BuZ3xoOTgvaGQ3LzEyNjgwMzcxOTI5MTE4LnBuZ3wzZjU1YzNmYmMzZDgxOTQ5NjBkZjU2ZThhNmUxZGMzY2E2ZjM3ZjM1OGMyZDA4YzhjNTBhNjUxZDRhMDlhZjgx" alt="태블릿" />
             </div>
-            <div id="information" colSpan={1}>
+            <div id="information">
                 <div>
                     <p className="equipment-name">
-                        <span>{tool_name}</span> &nbsp; 대여 가능
+                        <span>{tool_name}</span> &nbsp; {tool_state}
                     </p>
                     <p className="equipment-code">
                         품목 코드 : &nbsp; {tool_code} <br />
@@ -40,17 +43,34 @@ export default function DetailEquipment({ data }) {
                 </div>
             </div>
             <div id="information" style={{
-                paddingLeft: "100px"
-            }} colSpan={2}>
-                <div>
-                    <p className="equipment-name">
-                        <span>대여 정보</span>
-                    </p>
-                    <p className="equipment-code">
-                        대여자: &nbsp; 홍길동(학부생) <br />
-                        대여 기간: &nbsp; 2022 / 11 / 16 ~ 2022 / 11 / 30 (남은 기간 : 14일)
-                    </p>
-                </div>
+                paddingLeft: "100px",
+            }}>
+                {data.rental &&
+                    <div>
+                        <p className="equipment-name">
+                            <span>대여 정보</span>
+                        </p>
+                        <div className="equipment-code" style={{
+                            width: "100%",
+                        }}>
+                            <p style={{
+                                marginBottom: 0
+                            }}>
+                                대여자: &nbsp; {data.rental.user.user_name}(학부생) <br />
+                            </p>
+                            <p style={{
+                                textAlign: "right",
+                                marginTop: 0
+                            }}>
+                                대여 기간: &nbsp;
+                                {rental_date?.split("-")[0]} / {rental_date?.split("-")[1]} / {rental_date?.split("-")[2].slice(0, 2)} <br />
+                                ~ {rental_due_date?.split("-")[0]} / {rental_due_date?.split("-")[1]} / {rental_due_date?.split("-")[2].slice(0, 2)}
+                                <br />
+                                (남은 기간 : {new Date().getDate() - rental_due_date?.split("-")[2].slice(0, 2)}일)
+                            </p>
+                        </div>
+                    </div>
+                }
             </div>
             {/* <td>
                 <button>기자재 수리 요청</button>
