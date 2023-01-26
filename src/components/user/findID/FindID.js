824d7566c7d299b9db5653f0d451be3d99d73a3b
xodn_ghkd <hwa1023@mjc.ac.kr>
2023-01-26 18:59:56 +0900

    feat: 아이디 찾기 페이지

--- src/components/user/findID/FindID.js
+++ src/components/user/findID/FindID.js
@@ -1,12 +1,27 @@
+import axios from "axios";
+// import { authEmail } from "hooks/authEmail";
+import { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../signUp/SignUp.scss";
 
 export default function FindID() {
+    const authEmail = async (user_email, setAuth) => {
+        console.log(user_email)
+        await axios.get(`${process.env.REACT_APP_DOMAIN}/email/authEmail/${user_email}`)
+            .then((res) => {
+                console.log(res);
+            })
+    }
+    const navigate = useNavigate();
     const [input, setInput] = useState({
         "email": "",
         "atEmail": "",
         "authNumber": ""
     });
+    const [auth, setAuth] = useState();
+
+    const [userInfo, setUserInfo] = useState();
 
     const onChange = (e) => {
         const { name, value } = e.target;
@@ -15,42 +30,97 @@ export default function FindID() {
             [name]: value
         });
     }
+
+    const onFindID = async () => {
+        const { email: domain, atEmail } = input;
+        console.log(input);
+        const email = `${domain}@${atEmail}`;
+        await axios.get(`${process.env.REACT_APP_DOMAIN}/user/findId`, {
+            params: {
+                user_email: email
+            }
+        }).then((res) => {
+            console.log(res.data.result);
+            if (res.data.suc) {
+                setUserInfo(res.data.result);
+            }
+        })
+    }
+
     return (
         <div id="loginWrap">
             <div id="center">
-                <h1>아이디 찾기</h1>
-                <div id="id-email">
-                    <div id="email">
-                        <p>이메일</p>
-                        <input
-                            name="email"
-                            value={input.email}
-                            onChange={onChange}
-                            placeholder="이메일을 입력해주세요."
-                        />&nbsp; @ &nbsp;
-                        <input id="input-select-atEmail" />
-                        {/* <select onChange={onChange} value={input.atEmail} name="atEmail">
-                            <option>gmail.com</option>
-                            <option>mjc.ac.kr</option>
-                            <option>daum.net</option>
-                            <option>naver.com</option>
-                        </select> */}
+                {!userInfo ?
+                    <>
+                        <h1>아이디 찾기</h1>
+                        <div id="id-email">
+                            <div id="email">
+                                <p>이메일</p>
+                                <input
+                                    style={{
+                                        width: "240px",
+                                        marginRight: "16px"
+                                    }}
+                                    name="email"
+                                    value={input.email}
+                                    onChange={onChange}
+                                    placeholder="이메일을 입력해주세요."
+                                />@
+                                <input
+                                    style={{
+                                        width: "160px",
+                                        marginLeft: "16px"
+                                    }}
+                                    id="input-select-atEmail"
+                                    name="atEmail"
+                                    value={input.atEmail}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <div id="id">
+                                <p>인증번호</p>
+                                <input
+                                    style={{
+                                        width: "350px",
+                                        marginRight: "16px"
+                                    }}
+                                    name="authNumber"
+                                    onChange={onChange}
+                                    value={input.authNumber}
+                                    placeholder="인증번호를 입력해주세요."
+                                />
+                                <button
+                                    id="emailAuth"
+                                    onClick={() => {
+                                        console.log(`${input.email}@${input.atEmail}`);
+                                        authEmail(`${input.email}@${input.atEmail}`);
+                                    }}>
+                                    인증하기</button>
+                            </div>
+                        </div>
+                        <button
+                            onClick={onFindID}
+                            className={(input.authNumber && (input.email && input.atEmail)) ? "activeLoginBtn" : ""}
+                            disabled={!(input.authNumber && (input.email && input.atEmail))}>
+                            다음
+                        </button>
+                    </>
+                    :
+                    <div id="ID-View">
+                        <h1>아이디 찾기</h1>
+                        <div>
+                            <p>
+                                <span>{userInfo?.user_name}</span> 님의 아이디는
+                            </p>
+                            <p>
+                                <span>{userInfo?.user_id}</span> 입니다.
+                            </p>
+                        </div>
+                        <button onClick={() => navigate("/")} className="activeLoginBtn" style={{
+                            width: "540px"
+                        }}>로그인 페이지 가기</button>
                     </div>
-                    <div>
-                        <p>인증번호</p>
-                        <input
-                            name="authNumber"
-                            onChange={onChange}
-                            value={input.authNumber}
-                            placeholder="인증번호를 입력해주세요."
-                        />
-                    </div>
-                </div>
-                <button
-                    className={(input.authNumber && (input.email && input.atEmail)) ? "activeLoginBtn" : ""}
-                    disabled={!(input.authNumber && (input.email && input.atEmail))}>
-                    다음
-                </button>
+                }
             </div>
         </div>
     )
